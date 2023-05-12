16aee45ea15d6b304f5ab645d5fa896fb7eb8983
= <=>
2023-05-13 03:28:23 +0400

    create input container function - template literal

--- script.js
+++ script.js
@@ -15,54 +15,20 @@ decrementBtn.addEventListener("click", () => {
   itemQuatity.value--;
 });
 
-function createInputContainer(container) {
-  const div = document.createElement("div");
-  div.classList.add(
-    "input-group",
-    "mb-3",
-    "d-flex",
-    "flex-row",
-    "justify-content-between"
-  );
-
-  const inputGroupPrepend = document.createElement("div");
-  inputGroupPrepend.classList.add("input-group-prepend");
-  const span = document.createElement("span");
-  span.classList.add("item-name");
-  span.textContent = "Item name";
-  const decrementBtn = document.createElement("button");
-  decrementBtn.classList.add("btn", "btn-outline-secondary", "btn-decrement");
-  decrementBtn.setAttribute("type", "button");
-  decrementBtn.textContent = "-";
-  inputGroupPrepend.appendChild(span);
-  inputGroupPrepend.appendChild(decrementBtn);
-
-  const input = document.createElement("input");
-  input.setAttribute("type", "number");
-  input.classList.add("form-control");
-  input.setAttribute("placeholder", "");
-  input.setAttribute("aria-label", "");
-  input.setAttribute("aria-describedby", "basic-addon1");
-  input.setAttribute("value", "0");
-
-  const incrementBtn = document.createElement("button");
-  incrementBtn.classList.add("btn", "btn-outline-secondary", "btn-increment");
-  incrementBtn.setAttribute("type", "button");
-  incrementBtn.textContent = "+";
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("btn", "btn-danger");
-  deleteBtn.textContent = "X";
-
-  const itemPriceSpan = document.createElement("span");
-  itemPriceSpan.classList.add("item-price");
-  itemPriceSpan.textContent = "Item Price";
-
-  div.appendChild(inputGroupPrepend);
-  div.appendChild(input);
-  div.appendChild(incrementBtn);
-  div.appendChild(deleteBtn);
-  div.appendChild(itemPriceSpan);
-
-  container.appendChild(div);
+function createInputContainer() {
+  modalBody.innerHTML += `
+      <div class="input-group mb-3 d-flex flex-row justify-content-between">
+        <div class="input-group-prepend">
+          <span class="item-name">Item name</span>
+          <button class="btn btn-outline-secondary btn-decrement" type="button">-</button>
+        </div>
+        <input type="number" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" value="0">
+        <button class="btn btn-outline-secondary btn-increment" type="button">+</button>
+        <button class="btn btn-danger">X</button>
+        <span class="item-price">Item Price</span>
+      </div>
+    `;
 }
+
+createInputContainer();
+createInputContainer();
